7abb6220ab9811d50062b5476f394d4dd34d6cd7
Patrick Teivas <teivas.2011@mail.ru>
2018-04-01 20:21:35 +0300

    Commit
    Added: remove button, listings save
    Fixed: GotFocus delete

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -28,19 +28,37 @@ namespace WpfApp
     {
         public List<Toode> Tooted { get; set; }
         public List<Toode> Ostukorv { get; set; }
-        public string TempNimetus;
-        public int TempHind;
+        //public string TempNimetus;
+        //public int TempHind;
 
         public MainWindow()
         {
+            AppDomain.CurrentDomain.ProcessExit += new EventHandler(OnProcessExit);
+
             Tooted = new List<Toode>();
             Ostukorv = new List<Toode>();
 
+            UpdateTooted();
             InitializeComponent();
+
+            TootedList.ItemsSource = Tooted;
+
+            void OnProcessExit(object sender, EventArgs e)
+            {
+                string Text = "";
+                foreach (var item in Tooted)
+                {
+                    Text = Text + item.Nimetus + "\n" + item.Hind + "\n\n";
+                }
+                File.WriteAllText("../../../Tooted.txt", Text);
+            }
         }
 
         private void Lisa(object sender, RoutedEventArgs e)
         {
+            string TempNimetus;
+            int TempHind;
+
             TempNimetus = Nimetus.Text;
             TempNimetus = Nimetus.Text;
             bool result = int.TryParse(Hind.Text, out TempHind);
@@ -60,7 +78,7 @@ namespace WpfApp
                     bool State = true;
                     foreach (var item in Tooted)
                     {
-                        if(item.Nimetus == TempNimetus) State = false;
+                        if (item.Nimetus == TempNimetus) State = false;
                     }
                     if (State == true)
                     {
@@ -93,18 +111,12 @@ namespace WpfApp
                     Ostukorv.Add(Tooted[TootedList.SelectedIndex]);
                     Ostukorv[Ostukorv.Count - 1].Kogus++;
                 }
-
-                //string ValitudToode = (TootedList.SelectedItem as Toode).Nimetus;
-                ////Ostukorv = (from item in Ostukorv
-                ////            where item.Nimetus == ValitudToode
-                ////            select item.Kogus);
-                //Ostukorv = Ostukorv.Where(x => x.Nimetus == ValitudToode)
             }
             else
             {
                 MessageBox.Show("Valige midagi!", "Viga");
             }
-                    
+
         }
 
         private void Osta(object sender, RoutedEventArgs e)
@@ -113,7 +125,7 @@ namespace WpfApp
             int Summa = 0;
             foreach (var item in Ostukorv)
             {
-                Tsekk = Tsekk + "Nimetus: " + item.Nimetus + "\nKogus: " + item.Kogus + "\nHind: " + item.Hind +"EUR\n\n";
+                Tsekk = Tsekk + "Nimetus: " + item.Nimetus + "\nKogus: " + item.Kogus + "\nHind: " + item.Hind + "EUR\n\n";
                 Summa = Summa + item.Kogus * item.Hind;
             }
             Tsekk = Tsekk + "Kokku: " + Summa + "EUR";
@@ -127,16 +139,65 @@ namespace WpfApp
             e.Handled = regex.IsMatch(e.Text);
         }
 
-        public void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        public void Nimetus_GotFocus(object sender, RoutedEventArgs e)
         {
             TextBox tb = (TextBox)sender;
-            tb.Text = string.Empty;
+            if (tb.Text == "Nimetus") tb.Text = string.Empty;
+        }
+
+        public void Hind_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (tb.Text == "Hind") tb.Text = string.Empty;
         }
 
         private void TextBox_LostFocus(object sender, RoutedEventArgs e)
         {
             TextBox tb = (TextBox)sender;
-            if(tb.Text == "") tb.Text = tb.Name;
+            if (tb.Text == "") tb.Text = tb.Name;
         }
+
+        public void UpdateTooted()
+        {
+            string[] TootedText = new string[] { };
+
+            try
+            {
+                TootedText = File.ReadAllLines("../../../Tooted.txt");
+            }
+            catch (FileNotFoundException)
+            {
+                File.Create("../../../Tooted.txt");
+            }
+
+            int i = 0;
+            string TempNimetus = "";
+            int TempHind = 0;
+
+            foreach (var item in TootedText)
+            {
+                i++;
+                if (i == 1) TempNimetus = item;
+                else if (i == 2)
+                {
+                    TempHind = int.Parse(item);
+                    Tooted.Add(new Toode() { Nimetus = TempNimetus, Hind = TempHind, Kogus = 0 });
+                }
+                else if (i == 3)
+                {
+                    i = 0;
+                    //ToDo: Check
+                }
+            }
+
+            i = 0;
+            TempNimetus = null;
+            TempHind = 0;
+        }
+
+        //private void Remove(object sender, RoutedEventArgs e)
+        //{
+
+        //}
     }
 }
